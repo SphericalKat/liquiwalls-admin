a3bd2391753a949ae43a60443e0997d823d424d9
ATechnoHazard <amolele@gmail.com>
2020-10-26 22:00:53 +0530

    :sparkles: bootstrap with react-admin-firebase
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import {
+  FirebaseAuthProvider,
+  FirebaseDataProvider,
+} from "react-admin-firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const config = {
+  apiKey: "AIzaSyAO1LTfk5M5uSvWAXng_J790kmPmQyU0j8",
+  authDomain: "liquiwalls.firebaseapp.com",
+  databaseURL: "https://liquiwalls.firebaseio.com",
+  projectId: "liquiwalls",
+  storageBucket: "liquiwalls.appspot.com",
+  messagingSenderId: "2604914466",
+  appId: "1:2604914466:web:beef586aba2aceb35a377b",
+  measurementId: "G-6V386GF8CF",
+};
+
+// All options are optional
+const options = {
+  logging: true,
+};
+
+const dataProvider = FirebaseDataProvider(config, options);
+const authProvider = FirebaseAuthProvider(config, options);
+
+const App = () => (
+  <Admin dataProvider={dataProvider} authProvider={authProvider} >
+      <Resource name="walls" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
